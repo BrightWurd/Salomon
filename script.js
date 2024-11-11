6a14b20e6fbe175b139c429fd5563f4a694c16ad
God's Bright <godsbrightc@gmail.com>
2024-11-11 23:36:55 +0100

    Complete the menu bar and fixed some twicks in the page, like the jumbotron length for desktop view Mile 3

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
+let hoverSlide = $(".Navigation-buttons2");
 
+let menuBtn = $(".menu-btn").on("click", () => {
+  hoverSlide.addClass("move-in");
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
+$(".Navigation-buttons2").on("click", () => {
+  $(".Navigation-buttons2").removeClass("move-in");
+});
