c76cf952b1aff09d1503dad459192f554626a782
God's Bright <godsbrightc@gmail.com>
2024-11-16 03:28:20 +0100

    Complete website

--- script.js
+++ script.js
@@ -7,3 +7,29 @@ let menuBtn = $(".menu-btn").on("click", () => {
 $(".Navigation-buttons2").on("click", () => {
   $(".Navigation-buttons2").removeClass("move-in");
 });
+
+$(document).on("scroll", () => {
+  if ($(window).scrollTop() > 400) {
+    $("nav").addClass("blur");
+    $(".menu-btn,.search-btn").addClass("maker");
+  } else {
+    $("nav").removeClass("blur");
+    $(".menu-btn,.search-btn").removeClass("maker");
+  }
+});
+
+$(".register").on("click", () => {
+  window.location.href = "Masters.html";
+});
+
+$("#Cotonou").on("click", () => {
+  window.location.href = "AboutCotonou.html";
+});
+
+$("#Excursion").on("click", () => {
+  window.location.href = "Excursion.html";
+});
+
+$("#SchoolBuilding").on("click", () => {
+  window.location.href = "OurUniversity.html";
+});
